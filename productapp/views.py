0ac72b79dd7f5e69d6bb6b4495f7f75aad9ca363
Sourav Garai <sourav@Souravs-MacBook-Air.local>
2024-12-07 19:55:05 +0530

    Product View

---
+++ productapp/views.py
@@ -0,0 +1,9 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+# Create your views here.
+
+
+
+@api_view(['GET'])
+def getData(request):
+    return Response({"Products":"List of products"})
