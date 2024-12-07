0d39bed70f3b231a7d1d5a2a8e3c279c82efb554
Sourav Garai <sourav@Souravs-MacBook-Air.local>
2024-12-07 19:54:37 +0530

    Authentication code

---
+++ server/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, re_path, include
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    re_path('login',views.login),
+    re_path('signup',views.signup),
+    re_path('test_token',views.test_token),
+    path('',include('productapp.urls')),
+]
